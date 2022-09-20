02dfce21e8f96c9096429696e542a6f518c00867
petushka1 <nadya@devery.io>
2022-09-20 11:14:26 +0300

    fix linters error

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require_relative 'book'
 
 class App
-    attr_accessor :books, :games, :music_albums
-    def initialize
-        @books = []
-        @games = []
-        @music_albums = []
-    end
+  attr_accessor :books, :games, :music_albums
+
+  def initialize
+    @books = []
+    @games = []
+    @music_albums = []
+  end
 end
