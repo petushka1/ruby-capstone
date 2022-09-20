dfa310e559263c986ab5faee35afd1b340603ae5
c3c1l1a <cesswangumukima@gmail.com>
2022-09-20 18:08:36 +0800

    Add test for MusicAlbum class

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/music_album.rb'
+
+describe MusicAlbum do
+	describe '#new' do
+		it 'checks instance of MusicAlbum' do 
+			music_album = MusicAlbum.new(on_spotify:true)
+			expect(music_album.class).to eql MusicAlbum
+		end
+	end 
+end
