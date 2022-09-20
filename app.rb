015f94ebcd7f511d2c01195a3ddd997b6a1db991
petushka1 <nadya@devery.io>
2022-09-20 11:11:56 +0300

    add CLI set up

---
+++ app.rb
@@ -0,0 +1,10 @@
+require_relative 'book'
+
+class App
+    attr_accessor :books, :games, :music_albums
+    def initialize
+        @books = []
+        @games = []
+        @music_albums = []
+    end
+end
