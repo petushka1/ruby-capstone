aa775a75aad8625090a87a056b52dce7d3a8ab92
c3c1l1a <cesswangumukima@gmail.com>
2022-09-21 14:02:03 +0800

    Add tests for the genre class

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,6 @@
 class Genre
 	attr_accessor :name
+	attr_reader :items
 	def initialize(name)
 		@id = Random.rand(1...1000)
 		@name = name
@@ -7,5 +8,8 @@ class Genre
 	end
 
 	def add_item(item)
+		item.genre = 'test'
+		p item.genre
+		@items << item
 	end
 end
