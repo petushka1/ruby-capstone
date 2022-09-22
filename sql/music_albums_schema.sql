a5991831e2095bf670eb568c1fc22afcc830f96b
c3c1l1a <cesswangumukima@gmail.com>
2022-09-22 16:42:15 +0800

    Add schema for music_albums table

--- sql/music_albums_schema.sql
+++ sql/music_albums_schema.sql
@@ -1,4 +1,8 @@
 CREATE TABLE music_albums (
     id INT PRIMARY KEY NOT NULL,
     on_spotify BOOLEAN,
+    archived BOOLEAN,
+    publish_date DATE NOT NULL,
+    genre_id INT, 
+    FOREIGN KEY (genre_id) REFERENCES genre(id)
 ); 
