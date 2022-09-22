902d6160cddeca40db9921d474c19495b6822050
c3c1l1a <cesswangumukima@gmail.com>
2022-09-22 23:37:58 +0800

    Refactor list genres and albums

--- lib/music_parser.rb
+++ lib/music_parser.rb
@@ -4,6 +4,7 @@ require_relative 'music_album'
 module MusicParser
 	def add_music_album
 		puts "\nTo add a music album, please enter: "
+		album_name = get_input(['Name of album?: '])
 		published_date = get_input(['Publish date in the format [YYYY-MM-DD]: '])
 		on_spotify = get_input(['Is the album on spotify? [Y/n]: '])
 		puts "\nWhat genre is it? Choose and option below:"
@@ -23,7 +24,7 @@ module MusicParser
 		end
 
 		on_spotify = on_spotify.upcase == 'Y' 
-		music_album = MusicAlbum.new(published_date, on_spotify:on_spotify)
+		music_album = MusicAlbum.new(published_date, album_name, on_spotify:on_spotify)
 
 		pre_existing_genre = @music_albums.select do |item|
 			item.genre.name == genre
@@ -38,15 +39,18 @@ module MusicParser
 
 		genre.add_item(music_album)
 		@music_albums << music_album
+
+		puts "Music album added sucessfully!!\n\n"
 	end
 
 	def list_all_music_albums
 		return 'No music albums found' if @music_albums.empty?
 
 		puts "\n"
-		@music_albums.each_with_index do |music_album, i|
-			puts "#{i}: \tPUBLISHED ON: #{music_album.publish_date}, ON SPOTIFY?: #{music_album.on_spotify}, ", 
-					"\tGENRE: #{music_album.genre.name}, ARCHIVED?: #{music_album.archived}"
+		@music_albums.each_with_index do |album, i|
+			puts "#{i}: \tAlbum name: #{album.name}, PUBLISHED ON: #{album.publish_date},",
+					 "\tON SPOTIFY?: #{album.on_spotify}, ", 
+					"\tGENRE: #{album.genre.name}, ARCHIVED?: #{album.archived}"
 		end
 		puts "\n\n"
 	end
@@ -54,11 +58,13 @@ module MusicParser
 	def list_all_genres
 		@genres.each do |genre|
 			puts "\n#{genre.name}:"
-			genre.items.each_with_index do |music_album, i|
-				puts "#{i}: \tPUBLISHED ON: #{music_album.publish_date}, ON SPOTIFY?: #{music_album.on_spotify}, ", 
-					"\tGENRE: #{music_album.genre.name}, ARCHIVED?: #{music_album.archived}"
+			genre.items.each_with_index do |album, i|
+				puts "#{i}: \tAlbum name: #{album.name}, PUBLISHED ON: #{album.publish_date},",
+					 "\tON SPOTIFY?: #{album.on_spotify}, ", 
+					"\tGENRE: #{album.genre.name}, ARCHIVED?: #{album.archived}"
 			end
 		end
+		puts "\n\n"
 	end 
 
 	def get_input(questions = [])
