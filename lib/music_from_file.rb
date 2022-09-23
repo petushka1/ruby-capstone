cf60c5462660ccdb26cdc4906e3802523e6062c2
c3c1l1a <cesswangumukima@gmail.com>
2022-09-23 21:40:27 +0800

    Retrieve data from file

--- lib/music_from_file.rb
+++ lib/music_from_file.rb
@@ -3,27 +3,30 @@ require_relative './genre'
 require_relative './music_album'
 
 def get_music_albums(music_albums)
-  data = JSON.parse(File.read('./database/music_albums.json'))
+  if (File.file?('./database/music_albums.json')) 
+    data = JSON.parse(File.read('./database/music_albums.json'))
 
-
-  data.each do |item|
-    music_album = MusicAlbum.new(item["publish_date"], item["name"], on_spotify: item["on_spotify"])
-    music_album.id = item["id"]
-    music_album.genre = item["genre"]
-    music_albums << music_album
+    data.each do |item|
+      music_album = MusicAlbum.new(item["publish_date"], item["name"], on_spotify: item["on_spotify"])
+      music_album.id = item["id"]
+      music_album.genre = item["genre"]
+      music_albums << music_album
+    end
   end
 end
 
 def get_genres(app)
-  data = JSON.parse(File.read('./database/genres.json'))
-  data.each do |item|
-    genre = Genre.new(item["name"])
-    item["items"].each do |album|
+  if (File.file?('./database/music_albums.json')) 
+    data = JSON.parse(File.read('./database/genres.json'))
+    data.each do |item|
+      genre = Genre.new(item["name"])
+      app.music_albums.each do |album|
+        if (album.genre == genre.name) 
+          genre.add_item(album)
+        end
+      end
+      app.genres << genre
     end
-    
-    app.genres << {
-      name: item["name"],
-    }
   end
 end
 
