9ee14d00e98529c2770adc6f0568a37da1ddfb5b
Nadia Levina <chigiwowwow@gmail.com>
2022-09-24 09:37:35 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -26,7 +26,6 @@ class App
   def list_all_books
     return 'No books found' if @books.empty?
 
-    puts @books
     @books.each do |book|
       print "\n[BOOK][ID][#{book.id}] Publisher: #{book.publisher},"
       print "Publish date: #{book.publish_date}, Cover state: #{book.cover_state}"
